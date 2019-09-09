239934bc47ee3c335ebc8cba9ae201da1646b18b
LE Van Phuc <vanphuc@gmail.com>
2019-09-09 23:06:47 +0200

    use function component in answersUI

--- src/modules/question_answer/component/AddQuestion.js
+++ src/modules/question_answer/component/AddQuestion.js
@@ -1,90 +1,109 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import BasicComponent from "../../../common/abstract/component/BasicComponent";
-import Tabs from "../../../component/tabs/Tabs";
-import TypeQuestionTab from "./tabs/TypeQuestion";
-import TagsQuestionTab from "./tabs/TagsQuestion";
-import TitleQuestionTab from "./tabs/TitleQuestion";
-import DescriptionQuestionTab from "./tabs/DescriptionQuestion";
-import ReviewQuestionTab from "./tabs/ReviewQuestion";
+import PropTypes from 'prop-types';
+import BasicComponent from '../../../common/abstract/component/BasicComponent';
+import Tabs from '../../../component/tabs/Tabs';
+import TypeQuestionTab from './tabs/TypeQuestion';
+import TagsQuestionTab from './tabs/TagsQuestion';
+import TitleQuestionTab from './tabs/TitleQuestion';
+import DescriptionQuestionTab from './tabs/DescriptionQuestion';
+import ReviewQuestionTab from './tabs/ReviewQuestion';
 
-const propTypes = {
-    getSubCategoriesByCategory: PropTypes.func.isRequired,
-    createQuestion: PropTypes.func.isRequired,
+const AddQuestion = ({
+  history,
+  getSubCategoriesByCategory,
+  createQuestion,
+}) => {
+  const [categorySlug, setCategorySlug] = React.useState(null);
+  const [currentTab, setCurrentTab] = React.useState('Type');
+  const [subCategories, setSubCategories] = React.useState([]);
+  const [selectedTags, setSelectedTags] = React.useState([]);
+  const [titleEditted, setTitleEditted] = React.useState('');
+  const [bodyEditted, setBodyEditted] = React.useState('');
+
+  const setCategory = category => {
+    setCategorySlug(category);
+    setCurrentTab('Tags');
+    getSubCategoriesByCategory(category, setSubCategories);
+  };
+
+  const setTags = tags => {
+    setSelectedTags(tags);
+    setCurrentTab('Title');
+  };
+
+  const setTitle = title => {
+    const titleToSave = title.replace(/ +/g, ' ');
+    setTitleEditted(titleToSave.trim());
+  };
+
+  const setBody = val => {
+    setBodyEditted(val);
+    setCurrentTab('Description');
+  };
+
+  const goToTab = tab => {
+    setCurrentTab(tab);
+  };
+
+  const postQuestion = () => {
+    let slug = titleEditted.toLowerCase().replace(/[^a-z0-9 ]/g, '');
+    const question: Question = {
+      title: titleEditted,
+      body: bodyEditted,
+      categorySlug,
+      tags: JSON.stringify(selectedTags),
+      slug: slug.replace(/ /g, '-'),
+    };
+    createQuestion(question, history);
+  };
+
+  const isDisabled = !categorySlug;
+  return (
+    <section className="tabs-container pl3 pr3 pt5">
+      <Tabs activeTab={currentTab}>
+        <div label="Type">
+          <TypeQuestionTab category={categorySlug} next={setCategory} />
+        </div>
+        <div label="Tags" isDisabled={isDisabled}>
+          <TagsQuestionTab
+            subCategories={subCategories}
+            selectedTags={selectedTags}
+            previous={goToTab}
+            next={setTags}
+          />
+        </div>
+        <div label="Title" isDisabled={isDisabled}>
+          <TitleQuestionTab
+            title={titleEditted}
+            previous={goToTab}
+            next={goToTab}
+            setTitle={setTitle}
+          />
+        </div>
+        <div label="Description" isDisabled={isDisabled}>
+          <DescriptionQuestionTab
+            body={bodyEditted}
+            previous={goToTab}
+            next={goToTab}
+            setBody={setBody}
+          />
+        </div>
+        <div label="Review" isDisabled={isDisabled}>
+          <ReviewQuestionTab
+            title={titleEditted}
+            body={bodyEditted}
+            tags={selectedTags}
+            postQuestion={postQuestion}
+          />
+        </div>
+      </Tabs>
+    </section>
+  );
 };
-export default class AddQuestion extends BasicComponent {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            categorySlug: null,
-            currentTab: "Type",
-            subCategories: [],
-            selectedTags: [],
-            title: "",
-            body: "",
-        }
-    }
+AddQuestion.propTypes = {
+  getSubCategoriesByCategory: PropTypes.func.isRequired,
+  createQuestion: PropTypes.func.isRequired,
+};
 
-    setCategory = category => {
-        this.changeStateValues(new Map([['categorySlug', category], ['currentTab', 'Tags']]));
-        this.props.getSubCategoriesByCategory(category, this);
-    };
-
-    setTags = tags => {
-        this.changeStateValues(new Map([['selectedTags', tags], ['currentTab', 'Title']]));
-    };
-
-    setTitle = title => {
-        title = title.replace(/ +/g, ' ');
-        this.changeStateValue('title', title.trim());
-    };
-
-    setBody = body => {
-        this.changeStateValues(new Map([['body', body], ['currentTab', 'Description']]));
-    };
-
-    goToTab = (tab) => {
-        this.changeStateValue('currentTab', tab);
-    };
-
-    postQuestion = () => {
-        const { title, body, categorySlug, selectedTags } = this.state;
-        let slug = title.toLowerCase().replace(/[^a-z0-9 ]/g, "");
-        const question: Question = {
-            title: title,
-            body: body,
-            categorySlug: categorySlug,
-            tags: JSON.stringify(selectedTags),
-            slug: slug.replace(/ /g, '-'),
-        };
-        this.props.createQuestion(question, this.props.history)
-    };
-
-    render() {
-        const { currentTab, categorySlug, subCategories, selectedTags, title, body } = this.state;
-        const isDisabled = !categorySlug;
-        return (
-            <section className="tabs-container pl3 pr3 pt5">
-                <Tabs activeTab={currentTab}>
-                    <div label="Type">
-                        <TypeQuestionTab category={categorySlug} next={this.setCategory}/>
-                    </div>
-                    <div label="Tags" isDisabled={isDisabled}>
-                        <TagsQuestionTab subCategories={subCategories} selectedTags={selectedTags} previous={this.goToTab} next={this.setTags}/>
-                    </div>
-                    <div label="Title" isDisabled={isDisabled}>
-                        <TitleQuestionTab title={title} previous={this.goToTab} next={this.goToTab} setTitle={this.setTitle}/>
-                    </div>
-                    <div label="Description" isDisabled={isDisabled}>
-                        <DescriptionQuestionTab body={body} previous={this.goToTab} next={this.goToTab} setBody={this.setBody}/>
-                    </div>
-                    <div label="Review" isDisabled={isDisabled}>
-                        <ReviewQuestionTab title={title} body={body} tags={selectedTags} postQuestion={this.postQuestion}/>
-                    </div>
-                </Tabs>
-            </section>
-        )
-    }
-}
-AddQuestion.propTypes = propTypes;
+export default AddQuestion;
--- src/modules/question_answer/container/AnswersImpl.js
+++ src/modules/question_answer/container/AnswersImpl.js
@@ -1,14 +1,14 @@
-import connect from "react-redux/es/connect/connect";
-import AnswersUI from "../component/AnswersUI";
-import type {Answer} from "../../../domain/Answer";
-import CoreService from "../../../global/CoreService";
-import Result from "../../../global/Result";
-import MainPage from "../../home/component/content/MainPage";
-import RootScope from "../../../global/RootScope";
-import type {UsersVoteAnswers} from "../../../domain/UsersVoteAnswers";
-import type {Question} from "../../../domain/Question";
-import SweetAlert from "../../../global/SweetAlert";
-import ApplicationUtil from "../../../common/util/ApplicationUtil";
+import connect from 'react-redux/es/connect/connect';
+import AnswersUI from '../component/AnswersUI';
+import type { Answer } from '../../../domain/Answer';
+import CoreService from '../../../global/CoreService';
+import Result from '../../../global/Result';
+import MainPage from '../../home/component/content/MainPage';
+import RootScope from '../../../global/RootScope';
+import type { UsersVoteAnswers } from '../../../domain/UsersVoteAnswers';
+import type { Question } from '../../../domain/Question';
+import SweetAlert from '../../../global/SweetAlert';
+import ApplicationUtil from '../../../common/util/ApplicationUtil';
 
 const answerService = CoreService.answerService;
 const usersVoteService = CoreService.usersVoteService;
@@ -17,62 +17,90 @@ const questionService = CoreService.questionService;
 /**
  * The method ensure every user should login before continue answer the question
  * @param isAuthenticated: {Boolean} Is authenticated
- * @param _this: {AnswersUI} The answer UI
  * @param redirect: {any} The router redirect
  * @return {Function}
  */
-function leaveAnswerValidation(isAuthenticated: boolean, _this: AnswersUI, redirect: any) {
-    return () => {
-        if (!isAuthenticated) {
-            return redirect.push('/login');
-        }
-        _this.changeStateValue('leaveAnswer', true);
+function leaveAnswerValidation(
+  isAuthenticated: boolean,
+  setLeaveAnswer,
+  redirectTo
+) {
+  return () => {
+    if (!isAuthenticated) {
+      return redirectTo('/login');
     }
+    setLeaveAnswer(true);
+  };
 }
 
 /**
  * The method handles logic to create new answer
  * @param answerBody: {String} The answer body
  * @param questionId: {Number} The question Id
- * @param _this: {AnswersUI} The Answers UI
  * @return {Function}
  */
-function createNewAnswer(answerBody: string, questionId: number, _this: AnswersUI) {
-    return () => {
-        const descrLength: number = answerBody.length / 3;
-        const answerRequest: Answer = {
-            body: answerBody,
-            description: answerBody.substring(0, descrLength),
-            questionId: questionId,
-        };
-        answerService.create(answerRequest).then((result: Result) => {
-            if (result.success) {
-                let answers = _this.getDataFromState("answers") || [];
-                answers.unshift(result.data);
-                SweetAlert.show(SweetAlert.successAlertBuilder("Success!", "Leaved an answer"));
-                _this.changeStateValues(new Map([["answers", answers], ["answerBody", ""]]));
-            } else {
-                // To do: handle error
-                SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-            }
-        })
-    }
+function createNewAnswer(
+  answerBody: string,
+  questionId: number,
+  answersEditted,
+  setAnswersEditted,
+  setAnswerBody
+) {
+  return () => {
+    const descrLength: number = answerBody.length / 3;
+    const answerRequest: Answer = {
+      body: answerBody,
+      description: answerBody.substring(0, descrLength),
+      questionId: questionId,
+    };
+    answerService.create(answerRequest).then((result: Result) => {
+      if (result.success) {
+        const answers = answersEditted || [];
+        answers.unshift(result.data);
+        SweetAlert.show(
+          SweetAlert.successAlertBuilder('Success!', 'Leaved an answer')
+        );
+        setAnswersEditted(answers);
+        setAnswerBody('');
+      } else {
+        // To do: handle error
+        SweetAlert.show(
+          SweetAlert.errorAlertBuilder(
+            'Error!',
+            ApplicationUtil.getErrorMsg(result.data)
+          )
+        );
+      }
+    });
+  };
 }
 
-function approveAnswer(question: Question, answer: Answer, _this: AnswersUI) {
-    return () => {
-        _this.changeStateValue('disableApproveBtn', true);
-        questionService.doApproveAnswer(question.id, answer.id).then((result: Result) => {
-            if (result.success) {
-                question.hasAcceptedAnswer = answer.isTheBest = true;
-                _this.changeStateValue('disableApproveBtn', false);
-                _this.triggerUpdateQuestion(question);
-            } else {
-                // Todo: handle error
-                SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-            }
-        })
-    }
+function approveAnswer(
+  question: Question,
+  answer: Answer,
+  setDisableApproveBtn,
+  triggerUpdateQuestion
+) {
+  return () => {
+    setDisableApproveBtn(true);
+    questionService
+      .doApproveAnswer(question.id, answer.id)
+      .then((result: Result) => {
+        if (result.success) {
+          question.hasAcceptedAnswer = answer.isTheBest = true;
+          setDisableApproveBtn(false);
+          triggerUpdateQuestion(question);
+        } else {
+          // Todo: handle error
+          SweetAlert.show(
+            SweetAlert.errorAlertBuilder(
+              'Error!',
+              ApplicationUtil.getErrorMsg(result.data)
+            )
+          );
+        }
+      });
+  };
 }
 
 /**
@@ -80,63 +108,83 @@ function approveAnswer(question: Question, answer: Answer, _this: AnswersUI) {
  * @param answer: {Answer} The answer instance
  * @param isPositiveVote: {boolean} The value to check It's positive vote or not?
  * @param isVotedBefore: {boolean} The value to check It's voted before or not?
- * @param _this: {MainPage} The MainPage UI
  * @return {Function}
  */
-function handleVoteAnswer(answer: Answer, isPositiveVote: boolean, isVotedBefore: boolean, _this: MainPage) {
-    return () => {
-        if (!RootScope.userId) return _this.redirectTo('/login');
-        _this.changeStateValue('loader', {answerId: answer.id});
-        const data: UsersVoteAnswers = {
-            answerId: answer.id,
-            isPositiveVote: isPositiveVote,
-        };
-        if (isVotedBefore) {
-            data.id = answer.votes[0].id;
-            data.userId = answer.votes[0].userId;
-            usersVoteService.reVoteAnswer(data).then((result: Result) => {
-                if (result.success) {
-                    answer.votes[0].isPositiveVote = isPositiveVote;
-                    updateUIAfterVote(answer, isPositiveVote, isVotedBefore, _this);
-                } else {
-                    // Todo: Show error here
-                    _this.changeStateValue('loader', false);
-                    SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-                }
-            })
+function handleVoteAnswer(
+  answer: Answer,
+  isPositiveVote: boolean,
+  isVotedBefore: boolean,
+  setLoader,
+  redirectTo
+) {
+  return () => {
+    if (!RootScope.userId) return redirectTo('/login');
+    setLoader({ answerId: answer.id });
+    const data: UsersVoteAnswers = {
+      answerId: answer.id,
+      isPositiveVote: isPositiveVote,
+    };
+    if (isVotedBefore) {
+      data.id = answer.votes[0].id;
+      data.userId = answer.votes[0].userId;
+      usersVoteService.reVoteAnswer(data).then((result: Result) => {
+        if (result.success) {
+          answer.votes[0].isPositiveVote = isPositiveVote;
+          updateUIAfterVote(answer, isPositiveVote, isVotedBefore, setLoader);
         } else {
-            usersVoteService.voteAnswer(data).then((result: Result) => {
-                if (result.success) {
-                    answer.votes = [result.data];
-                    updateUIAfterVote(answer, isPositiveVote, isVotedBefore, _this);
-                } else {
-                    // Todo: Show error here
-                    _this.changeStateValue('loader', false);
-                    SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-                }
-            })
+          // Todo: Show error here
+          setLoader(false);
+          SweetAlert.show(
+            SweetAlert.errorAlertBuilder(
+              'Error!',
+              ApplicationUtil.getErrorMsg(result.data)
+            )
+          );
         }
-    }
-}
-
-function updateUIAfterVote(answer: Answer, isPositiveVote: boolean, isVotedBefore: boolean, _this: MainPage): void {
-    const times: number = isVotedBefore ? 2 : 1;
-    if (isPositiveVote) {
-        answer.numberOfVotes += times;
+      });
     } else {
-        answer.numberOfVotes -= times;
+      usersVoteService.voteAnswer(data).then((result: Result) => {
+        if (result.success) {
+          answer.votes = [result.data];
+          updateUIAfterVote(answer, isPositiveVote, isVotedBefore, setLoader);
+        } else {
+          // Todo: Show error here
+          setLoader(false);
+          SweetAlert.show(
+            SweetAlert.errorAlertBuilder(
+              'Error!',
+              ApplicationUtil.getErrorMsg(result.data)
+            )
+          );
+        }
+      });
     }
-    _this.changeStateValue('loader', false);
+  };
 }
 
-const mapStateToProps = (store) => {
-    return {
-        isAuthenticated: store.AppAuth.isAuthenticated,
-    }
+function updateUIAfterVote(
+  answer: Answer,
+  isPositiveVote: boolean,
+  isVotedBefore: boolean,
+  setLoader
+): void {
+  const times: number = isVotedBefore ? 2 : 1;
+  if (isPositiveVote) {
+    answer.numberOfVotes += times;
+  } else {
+    answer.numberOfVotes -= times;
+  }
+  setLoader(false);
+}
+
+const mapStateToProps = store => {
+  return {
+    isAuthenticated: store.AppAuth.isAuthenticated,
+  };
 };
 
 const AnswersImpl = connect(
-    mapStateToProps,
-    { leaveAnswerValidation, createNewAnswer, handleVoteAnswer, approveAnswer }
+  mapStateToProps,
+  { leaveAnswerValidation, createNewAnswer, handleVoteAnswer, approveAnswer }
 )(AnswersUI);
 export default AnswersImpl;
