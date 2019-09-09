1135508783c915eec4c0018a855194b37721adbf
LE Van Phuc <vanphuc@gmail.com>
2019-09-09 23:27:20 +0200

    use hooks in view question

--- src/modules/question_answer/component/EditQuestion.js
+++ src/modules/question_answer/component/EditQuestion.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import BasicComponent from "../../../common/abstract/component/BasicComponent";
 
-export default class EditQuestion extends BasicComponent {
-    render() {
-        return (
-            <section>Edit Question UI Here</section>
-        )
-    }
-}
+const EditQuestion = ({}) => {
+  return <section>Edit Question UI Here</section>;
+};
+
+export default EditQuestion;
--- src/modules/question_answer/container/ViewQuestionImpl.js
+++ src/modules/question_answer/container/ViewQuestionImpl.js
@@ -1,13 +1,13 @@
-import connect from "react-redux/es/connect/connect";
-import ViewQuestion from "../component/ViewQuestion";
-import Result from "../../../global/Result";
-import CoreService from "../../../global/CoreService";
-import type {Question} from "../../../domain/Question";
-import MainPage from "../../home/component/content/MainPage";
-import RootScope from "../../../global/RootScope";
-import type {UsersVoteQuestions} from "../../../domain/UsersVoteQuestions";
-import SweetAlert from "../../../global/SweetAlert";
-import ApplicationUtil from "../../../common/util/ApplicationUtil";
+import connect from 'react-redux/es/connect/connect';
+import ViewQuestion from '../component/ViewQuestion';
+import Result from '../../../global/Result';
+import CoreService from '../../../global/CoreService';
+import type { Question } from '../../../domain/Question';
+import MainPage from '../../home/component/content/MainPage';
+import RootScope from '../../../global/RootScope';
+import type { UsersVoteQuestions } from '../../../domain/UsersVoteQuestions';
+import SweetAlert from '../../../global/SweetAlert';
+import ApplicationUtil from '../../../common/util/ApplicationUtil';
 
 const questionService = CoreService.questionService;
 const answerService = CoreService.answerService;
@@ -16,19 +16,23 @@ const usersVoteService = CoreService.usersVoteService;
 /**
  * The method handles to get question detail by Id
  * @param slug: {String} The question slug
- * @param _this: {ViewQuestion} The question detail UI
  * @return {Function}
  */
-function getQuestionDetail(slug: string, _this: ViewQuestion) {
-    return () => {
-        questionService.findOneBySlug(slug).then((result: Result) => {
-            if (result.success && result.data && Object.keys(result.data).length > 0) {
-                _this.changeStateValues(new Map([['question', result.data], ['answers', result.data.answers]]));
-            } else {
-                _this.redirectTo('/');
-            }
-        })
-    }
+function getQuestionDetail(slug: string, setQuestion, setAnswers, redirectTo) {
+  return () => {
+    questionService.findOneBySlug(slug).then((result: Result) => {
+      if (
+        result.success &&
+        result.data &&
+        Object.keys(result.data).length > 0
+      ) {
+        setQuestion(result.data);
+        setAnswers(result.data.answers);
+      } else {
+        redirectTo('/');
+      }
+    });
+  };
 }
 
 /**
@@ -38,16 +42,18 @@ function getQuestionDetail(slug: string, _this: ViewQuestion) {
  * @return {Function}
  */
 function loadMoreAnswers(questionId: number, _this: ViewQuestion) {
-    return () => {
-        let answers = _this.getDataFromState("answers") || [];
-        const filter = {skip: answers.length};
-        answerService.getAnswersByQuestionId(questionId, filter).then((result: Result) => {
-            if (result.success) {
-                answers = answers.concat(result.data);
-                _this.changeStateValue('answers', answers);
-            }
-        })
-    }
+  return () => {
+    let answers = _this.getDataFromState('answers') || [];
+    const filter = { skip: answers.length };
+    answerService
+      .getAnswersByQuestionId(questionId, filter)
+      .then((result: Result) => {
+        if (result.success) {
+          answers = answers.concat(result.data);
+          _this.changeStateValue('answers', answers);
+        }
+      });
+  };
 }
 
 /**
@@ -55,57 +61,77 @@ function loadMoreAnswers(questionId: number, _this: ViewQuestion) {
  * @param question: {Question} The question instance
  * @param isPositiveVote: {boolean} The value to check It's positive vote or not?
  * @param isVotedBefore: {boolean} The value to check It's voted before or not?
- * @param _this: {MainPage} The MainPage UI
  * @return {Function}
  */
-function handleVoteQuestion(question: Question, isPositiveVote: boolean, isVotedBefore: boolean, _this: MainPage) {
-    return () => {
-        if (!RootScope.userId) return _this.redirectTo('/login');
-        _this.changeStateValue('loader', {questionId: question.id});
-        const data: UsersVoteQuestions = {
-            questionId: question.id,
-            isPositiveVote: isPositiveVote,
-        };
-        if (isVotedBefore) {
-            data.id = question.votes[0].id;
-            data.userId = question.votes[0].userId;
-            usersVoteService.reVoteQuestion(data).then((result: Result) => {
-                if (result.success) {
-                    question.votes[0].isPositiveVote = isPositiveVote;
-                    updateUIAfterVote(question, isPositiveVote, isVotedBefore, _this);
-                } else {
-                    // Todo: Show error here
-                    _this.changeStateValue('loader', false);
-                    SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-                }
-            })
+function handleVoteQuestion(
+  question: Question,
+  isPositiveVote: boolean,
+  isVotedBefore: boolean,
+  redirectTo,
+  setLoader
+) {
+  return () => {
+    if (!RootScope.userId) return redirectTo('/login');
+    setLoader({ questionId: question.id });
+    const data: UsersVoteQuestions = {
+      questionId: question.id,
+      isPositiveVote: isPositiveVote,
+    };
+    if (isVotedBefore) {
+      data.id = question.votes[0].id;
+      data.userId = question.votes[0].userId;
+      usersVoteService.reVoteQuestion(data).then((result: Result) => {
+        if (result.success) {
+          question.votes[0].isPositiveVote = isPositiveVote;
+          updateUIAfterVote(question, isPositiveVote, isVotedBefore, setLoader);
         } else {
-            usersVoteService.voteQuestion(data).then((result: Result) => {
-                if (result.success) {
-                    question.votes = [result.data];
-                    updateUIAfterVote(question, isPositiveVote, isVotedBefore, _this);
-                } else {
-                    // Todo: Show error here
-                    _this.changeStateValue('loader', false);
-                    SweetAlert.show(SweetAlert.errorAlertBuilder('Error!',  ApplicationUtil.getErrorMsg(result.data)));
-                }
-            })
+          // Todo: Show error here
+          setLoader(false);
+          SweetAlert.show(
+            SweetAlert.errorAlertBuilder(
+              'Error!',
+              ApplicationUtil.getErrorMsg(result.data)
+            )
+          );
         }
+      });
+    } else {
+      usersVoteService.voteQuestion(data).then((result: Result) => {
+        if (result.success) {
+          question.votes = [result.data];
+          updateUIAfterVote(question, isPositiveVote, isVotedBefore, setLoader);
+        } else {
+          // Todo: Show error here
+          setLoader(false);
+          SweetAlert.show(
+            SweetAlert.errorAlertBuilder(
+              'Error!',
+              ApplicationUtil.getErrorMsg(result.data)
+            )
+          );
+        }
+      });
     }
+  };
 }
 
-function updateUIAfterVote(question: Question, isPositiveVote: boolean, isVotedBefore: boolean, _this: MainPage): void {
-    const times: number = isVotedBefore ? 2 : 1;
-    if (isPositiveVote) {
-        question.numberOfVotes += times;
-    } else {
-        question.numberOfVotes -= times;
-    }
-    _this.changeStateValue('loader', false);
+function updateUIAfterVote(
+  question: Question,
+  isPositiveVote: boolean,
+  isVotedBefore: boolean,
+  setLoader
+): void {
+  const times: number = isVotedBefore ? 2 : 1;
+  if (isPositiveVote) {
+    question.numberOfVotes += times;
+  } else {
+    question.numberOfVotes -= times;
+  }
+  setLoader(false);
 }
 
 const ViewQuestionImpl = connect(
-    null,
-    {getQuestionDetail, loadMoreAnswers, handleVoteQuestion}
+  null,
+  { getQuestionDetail, loadMoreAnswers, handleVoteQuestion }
 )(ViewQuestion);
 export default ViewQuestionImpl;
