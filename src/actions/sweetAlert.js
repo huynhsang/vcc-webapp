0d7fcf2f862b9da0475ea7d38bd51595995560c8
LE Van Phuc <vanphuc@gmail.com>
2019-09-15 18:49:33 +0200

    [T-30] Create Sweet alert action

--- src/actions/sweetAlert.js
+++ src/actions/sweetAlert.js
@@ -1,11 +1,47 @@
 import { createAction } from 'redux-starter-kit';
-import actionsNames from './actionNames';
-import SweetAlert from "../global/SweetAlert";
+import actionsNames from '../constants/action-names.constant';
 
-const {
-	SHOW_ALERT,
-	HIDE_ALERT
-} = actionsNames;
+import { SweetType } from '../constants/sweet-alert.constant';
+
+const { SHOW_ALERT, HIDE_ALERT } = actionsNames;
+
+const alertBuilder = (
+    title: string,
+    type: string,
+    text: string,
+    confirmButtonText: string,
+    onConfirm: void
+) => ({
+    show: true,
+    title,
+    type,
+    text,
+    showCanceltButton: false,
+    confirmButtonText,
+    cancelButtonText: null,
+    onConfirm,
+    onCancel: null,
+});
+
+const confirmAlertBuilder = (
+    title: string,
+    type: string,
+    text: string,
+    confirmButtonText: string,
+    cancelButtonText: string,
+    onConfirm: void,
+    onCancel: void
+) => ({
+    show: true,
+    title,
+    type,
+    text,
+    showCanceltButton: true,
+    confirmButtonText,
+    cancelButtonText,
+    onConfirm,
+    onCancel,
+});
 
 export const showAlertAction = createAction(SHOW_ALERT);
 export const hideAlertAction = createAction(HIDE_ALERT);
