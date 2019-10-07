5b42798227a350b8b7ec586ba947ccb06cd3b484
LE Van Phuc <vanphuc1195@gmail.com>
2019-10-07 22:03:55 +0200

    [T-39] Fix App render  before verifying  cookie

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -21,6 +21,8 @@ import { ConnectedRouter } from 'connected-react-router';
 import { history } from '../../configureStore';
 
 const App = ({ auth, uppdateAuthenticate }) => {
+    const [isVerifiedUser, setIsVerifiedUser] = React.useState(false);
+
     React.useEffect(() => {
         RootScope.token = CookieHelper.getCookie(CookieConstant.jwtTokenName);
         RootScope.userId = Number(
@@ -29,9 +31,16 @@ const App = ({ auth, uppdateAuthenticate }) => {
         if (RootScope.token && RootScope.userId) {
             getCurrentUser().then(() => {
                 uppdateAuthenticate();
+                setIsVerifiedUser(true);
             });
+        } else {
+            setIsVerifiedUser(true);
         }
     }, []);
+
+    if (!isVerifiedUser) {
+        return <div />;
+    }
 
     const classWrapper: string = auth.isAuthenticated
         ? 'wrap-login'
