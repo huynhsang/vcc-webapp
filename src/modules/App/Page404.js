cd67609f399feb3a7da3d16296a25beac57cc18f
LE Van Phuc <vanphuc1195@gmail.com>
2019-10-18 22:18:00 +0200

    [UPDATE] Refont Page 404

--- src/modules/App/Page404.js
+++ src/modules/App/Page404.js
@@ -1,43 +1,28 @@
 import React from 'react';
-import BasicComponent from '../../common/abstract/component/BasicComponent';
+import styled from 'styled-components';
 import ErrorImageV2 from '../../static/resources/img/errors/error-404-v2.png';
 import ErrorImageV3 from '../../static/resources/img/errors/error-404-v3.png';
 import RootScope from '../../global/RootScope';
+
+const Wrapper = styled.div`
+    margin: 30px;
+`;
 
 const Page404 = ({}) => {
     //TODO: add button comback
 
     const ErrorImage = RootScope.currentUser ? ErrorImageV2 : ErrorImageV3;
     return (
-        <div id="error-page">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-12">
-                        <div id="error-box">
-                            <div className="row">
-                                <div className="col-xs-12">
-                                    <div id="error-box-inner">
-                                        <img
-                                            src={ErrorImage}
-                                            alt="Have you seen this page?"
-                                        />
-                                    </div>
-                                    <h1>ERROR 404</h1>
-                                    <p>
-                                        Page not found.
-                                        <br />
-                                        If you find this page, let us know.
-                                    </p>
-                                    <p>
-                                        Go back to <a href="/">homepage</a>.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <Wrapper>
+            <div id="error-box-inner">
+                <img src={ErrorImage} alt="Have you seen this page?" />
             </div>
-        </div>
+            <h1>ERROR 404</h1>
+            <p>Page not found.</p>
+            <p>
+                Go back to <a href="/">homepage</a>.
+            </p>
+        </Wrapper>
     );
 };
 
