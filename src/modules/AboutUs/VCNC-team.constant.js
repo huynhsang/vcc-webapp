6c17451b53e99f96abc0c311af2c7ed1120e03e5
LE Van Phuc <vanphuc1195@gmail.com>
2019-11-12 00:03:18 +0100

    [T-71] Update team photo

--- src/modules/AboutUs/VCNC-team.constant.js
+++ src/modules/AboutUs/VCNC-team.constant.js
@@ -1,29 +1,34 @@
-import TestImage from '../../static/resources/img/garelly-avatar-3.jpg';
+import NguyetImage from '../../images/team/nguyet.png';
+import PhucImage from '../../images/team/phuc.png';
+import SangImage from '../../images/team/sang.png';
+import ToanImage from '../../images/team/toan.png';
+import NguyenImage from '../../images/team/nguyen.png';
+
 export const team = [
     {
-        img: TestImage,
-        name: 'A',
+        img: NguyetImage,
+        name: 'Nguyet',
+        role: 'Business'
+    },
+    {
+        img: PhucImage,
+        name: 'Phuc',
+        role: 'Dev'
+    },
+    {
+        img: SangImage,
+        name: 'Sang',
+        role: 'Dev'
+    },
+    {
+        img: ToanImage,
+        name: 'Toan',
+        role: 'Data'
+    },
+    {
+        img: NguyenImage,
+        name: 'Nguyen',
         role: 'Makerter'
-    },
-    {
-        img: TestImage,
-        name: 'B',
-        role: 'IT'
-    },
-    {
-        img: TestImage,
-        name: 'C',
-        role: 'IT'
-    },
-    {
-        img: TestImage,
-        name: 'D',
-        role: 'IT'
-    },
-    {
-        img: TestImage,
-        name: 'E',
-        role: 'IT'
     },
 
 ];
