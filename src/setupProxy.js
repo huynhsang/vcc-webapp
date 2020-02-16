caf8dd5a2d1f93ccf362416a75102066f152a482
LE Van Phuc <vp.le@kumullus.com>
2020-02-16 12:13:46 +0100

    [Update] add cors to proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,19 +2,16 @@ const proxy = require('http-proxy-middleware');
 
 const { REACT_APP_SOCIAL_LOGIN_API_URL } = process.env;
 
+const proxyData = {
+    target: REACT_APP_SOCIAL_LOGIN_API_URL,
+    changeOrigin: true,
+    onProxyReq(proxyReq) {
+        if (proxyReq.getHeader('origin')) {
+            proxyReq.setHeader('origin', REACT_APP_SOCIAL_LOGIN_API_URL);
+        }
+    }
+};
+
 module.exports = function(app) {
-    app.use(
-        '/auth/google',
-        proxy({
-            target: `${REACT_APP_SOCIAL_LOGIN_API_URL}`,
-            changeOrigin: true
-        })
-    );
-    app.use(
-        '/auth/facebook',
-        proxy({
-            target: `${REACT_APP_SOCIAL_LOGIN_API_URL}`,
-            changeOrigin: true
-        })
-    );
+    app.use('/auth', proxy({ ...proxyData }));
 };
