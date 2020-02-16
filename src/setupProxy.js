e1a9028b6c5bd3e36ae0c0591a90ca44d330cea5
LE Van Phuc <vp.le@kumullus.com>
2020-02-16 22:18:41 +0100

    [Test] Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,19 +4,19 @@ const { REACT_APP_SOCIAL_LOGIN_API_URL } = process.env;
 
 module.exports = function(app) {
     app.use(
-        '/auth/',
+        '/auth/google',
         proxy({
-            target: REACT_APP_SOCIAL_LOGIN_API_URL,
-            changeOrigin: true,
-            onProxyReq(proxyReq) {
-                if (proxyReq.getHeader('origin')) {
-                    proxyReq.setHeader(
-                        'origin',
-                        REACT_APP_SOCIAL_LOGIN_API_URL
-                    );
-                }
-            },
-            logLevel: 'debug'
+            target: `${REACT_APP_SOCIAL_LOGIN_API_URL}/auth/google`,
+            // changeOrigin: true,
+            // onProxyReq(proxyReq) {
+            //     if (proxyReq.getHeader('origin')) {
+            //         proxyReq.setHeader(
+            //             'origin',
+            //             REACT_APP_SOCIAL_LOGIN_API_URL
+            //         );
+            //     }
+            // },
+            // logLevel: 'debug'
         })
     );
 };
