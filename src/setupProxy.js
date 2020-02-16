336a25b42f29a1f4b05762a3c137951afab575ed
LE Van Phuc <vp.le@kumullus.com>
2020-02-16 09:40:09 +0100

    [Update] URL Social login

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,19 @@
 const proxy = require('http-proxy-middleware');
 
-const { REACT_APP_API_URL } = process.env;
+const { REACT_APP_SOCIAL_LOGIN_API_URL } = process.env;
 
 module.exports = function(app) {
     app.use(
         '/auth/google',
         proxy({
-            target: `${REACT_APP_API_URL}`,
+            target: `${REACT_APP_SOCIAL_LOGIN_API_URL}`,
             changeOrigin: true
         })
     );
     app.use(
         '/auth/facebook',
         proxy({
-            target: `${REACT_APP_API_URL}`,
+            target: `${REACT_APP_SOCIAL_LOGIN_API_URL}`,
             changeOrigin: true
         })
     );
