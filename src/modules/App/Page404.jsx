3cc29057cf4a67d1b9860676179912b5fbf071be
LE Van Phuc <vp.le@kumullus.com>
2020-08-11 20:16:55 +0200

    delete useless

--- src/modules/App/Page404.jsx
+++ src/modules/App/Page404.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import ErrorImageV2 from '../../images/error-404-v2.png';
-// import ErrorImageV3 from '../../images/error-404-v3.png';
 
 const Wrapper = styled.div`
     margin: 30px;
