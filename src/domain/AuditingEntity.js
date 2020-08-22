3846df89ba3ff73c802fbae18ef284469bdf71b6
LE Van Phuc <vp.le@kumullus.com>
2020-08-22 21:32:11 +0200

    remove useless

--- src/domain/AuditingEntity.js
+++
@@ -1,8 +0,0 @@
-import type {User} from './User';
-
-export interface AuditingEntity {
-	createdBy: User;
-	createdOn: Date;
-	updatedBy: User;
-	updatedOn: Date;
-}
--- src/domain/Category.js
+++
@@ -1,8 +0,0 @@
-import type {AuditingEntity} from './AuditingEntity';
-
-export interface Category extends AuditingEntity {
-    slug: string;
-    nameEn: string;
-    nameVi: string;
-    numberOfQuestions: number;
-}
